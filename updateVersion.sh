14f26f283fecfe6afc795ad2500d19d95d32b9d4
ktc <ktc@statsbiblioteket.dk>
2015-02-10 14:43:13 +0100

    update changelog and message examples prior to release

--- updateVersion.sh
+++ updateVersion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-FROM_VERSION=26
-TO_VERSION=27
+FROM_VERSION=27
+TO_VERSION=28
 
 sed -i "s/version=\"$FROM_VERSION\"/version=\"$TO_VERSION\"/g" src/main/resources/examples/messages/*.xml
 sed -i "s/version=\"$FROM_VERSION\"/version=\"$TO_VERSION\"/g" src/main/resources/examples/data/*.xml
