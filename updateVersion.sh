4304f37fedf2de72634f9e2817de373301e8b672
ktc <ktc@statsbiblioteket.dk>
2016-02-15 11:16:00 +0100

    Update message versions

--- updateVersion.sh
+++ updateVersion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-FROM_VERSION=27
-TO_VERSION=28
+FROM_VERSION=28
+TO_VERSION=29
 
 sed -i "s/version=\"$FROM_VERSION\"/version=\"$TO_VERSION\"/g" src/main/resources/examples/messages/*.xml
 sed -i "s/version=\"$FROM_VERSION\"/version=\"$TO_VERSION\"/g" src/main/resources/examples/data/*.xml
