da5f7ce6a1427484c16f919cffc166d359f838dc
Kim Christensen <ktc@kb.dk>
2019-05-20 14:45:46 +0200

    Fix path for updating examples in updateversion script

--- updateVersion.sh
+++ updateVersion.sh
@@ -3,5 +3,5 @@
 FROM_VERSION=28
 TO_VERSION=29
 
-sed -i "s/version=\"$FROM_VERSION\"/version=\"$TO_VERSION\"/g" src/main/resources/examples/messages/*.xml
-sed -i "s/version=\"$FROM_VERSION\"/version=\"$TO_VERSION\"/g" src/main/resources/examples/data/*.xml
+sed -i "s/version=\"$FROM_VERSION\"/version=\"$TO_VERSION\"/g" bitrepository-message-xml-xsd/src/main/resources/examples/messages/*.xml
+sed -i "s/version=\"$FROM_VERSION\"/version=\"$TO_VERSION\"/g" bitrepository-message-xml-xsd/src/main/resources/examples/data/*.xml
